5cfe2264ddb9dd4fbcf66f8f9523cfe073036773
RaulMurillo <ramurimontero@gmail.com>
2024-03-06 01:47:34 +0100

    conky installer and setup script

---
+++ conky_setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# install conky and dependencies
+sudo add-apt-repository ppa:teejee2008/foss
+sudo apt update
+sudo apt install -y conky-all conky-manager2
+
+# Config conky
+
+echo '[Desktop Entry]
+Type=Application
+Exec=sh "~/.conky/conky-startup.sh"
+Hidden=false
+NoDisplay=false
+X-GNOME-Autostart-enabled=true
+Name[en_IN]=Conky
+Name=Conky
+Comment[en_IN]=
+Comment=' > ~/.config/autostart/conky.desktop
+
+cp -r conky ~/.conky
+
